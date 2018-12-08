617bb241108dd375c58bfdf34f6268964d018298
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2018-12-07 18:28:59 -0600

    Copy files to processed dir

--- preprocess.sh
+++ preprocess.sh
@@ -4,7 +4,9 @@ unzip raw_data.zip
 
 ##### SELECT LABELS TO BE USED FOR PREDICTION #####
 python -m utils.select_labels --min_genes_per_class 200
+cp data/mashup/raw/annotations/human/reduced_adjacency-22.txt cge/
 python -m utils.select_labels --min_genes_per_class 30
+cp data/mashup/raw/annotations/human/reduced_adjacency-211.txt cge/
 
 ##### PREPROCESS HOMOLOGS FILE #####
 python -m utils.preprocess_homologs --data_dir ./data --output_dir ./data
